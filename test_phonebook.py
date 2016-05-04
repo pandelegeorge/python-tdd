524d648c274de59c8a9be74d3918d693dc848bd4
pandelegeorge <pandelegeorge@yahoo.com>
2016-05-04 18:12:11 -0400

    added a new unitest for key missing

--- test_phonebook.py
+++ test_phonebook.py
@@ -8,3 +8,7 @@ class PhonebookTest(unittest.TestCase):
         phonebook = Phonebook()
         phonebook.add("Bob","12345")
         self.assertEqual("12345",phonebook.lookup("Bob"))
+    def test_missing_entry_raises_KeyError(self):
+        phonebook = Phonebook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
