cd895ff4c2dfe79d036d7a58d95714c66c331491
pandelegeorge <pandelegeorge@yahoo.com>
2017-05-04 20:58:59 +0300

    refactor the all unit tests and add setup Fixture

--- test_phonebook.py
+++ test_phonebook.py
@@ -3,20 +3,17 @@ from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
 
-    def test_create_phonebook(self):
-        phonebook = Phonebook()
+    def setUp(self):
+        self.phonebook = Phonebook()
 
     def test_lookup_entry_by_name(self):
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
+        self.phonebook.add("Bob", "12345")
         self.assertEqual("12345", phonebook.lookup("Bob"))
 
     def test_missing_entry_raises_KeyError(self):
-        phonebook = Phonebook()
         with self.assertRaises((KeyError)):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
     @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
-        phonebook = Phonebook()
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
