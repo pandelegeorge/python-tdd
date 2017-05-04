565fcbef285a28b1f827839452c92dae3b9b2d82
pandelegeorge <pandelegeorge@yahoo.com>
2017-05-04 21:03:36 +0300

    fixing an issue by adding self previously test failed

--- test_phonebook.py
+++ test_phonebook.py
@@ -8,7 +8,7 @@ class PhonebookTest(unittest.TestCase):
 
     def test_lookup_entry_by_name(self):
         self.phonebook.add("Bob", "12345")
-        self.assertEqual("12345", phonebook.lookup("Bob"))
+        self.assertEqual("12345", self.phonebook.lookup("Bob"))
 
     def test_missing_entry_raises_KeyError(self):
         with self.assertRaises((KeyError)):
